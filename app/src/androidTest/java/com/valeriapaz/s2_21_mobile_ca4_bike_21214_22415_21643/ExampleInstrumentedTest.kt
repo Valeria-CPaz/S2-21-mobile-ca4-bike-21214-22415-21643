dbe32dcb5d4ce584b583742db9b377047abc8c7e
Valeria Paz <21214@student.dorset-college.ie>
2021-04-06 14:40:46 +0100

    commit

---
+++ app/src/androidTest/java/com/valeriapaz/s2_21_mobile_ca4_bike_21214_22415_21643/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.valeriapaz.s2_21_mobile_ca4_bike_21214_22415_21643
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.valeriapaz.s2_21_mobile_ca4_bike_21214_22415_21643", appContext.packageName)
+    }
+}
---
+++ app/src/test/java/com/valeriapaz/s2_21_mobile_ca4_bike_21214_22415_21643/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.valeriapaz.s2_21_mobile_ca4_bike_21214_22415_21643
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
