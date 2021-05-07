bd0a2cd8cbc81109524aaa369b0f8651a01dfb59
alexsonsilva <21643@student.dorset-college.ie>
2021-05-07 16:41:10 +0100

    Final commit

---
+++ app/src/main/java/com/valeriapaz/s2_21_mobile_ca4_bike_21214_22415_21643/LoginActivity.java
@@ -0,0 +1,179 @@
+package com.valeriapaz.s2_21_mobile_ca4_bike_21214_22415_21643
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+
+import kotlinx.android.synthetic.main.activity_j_c_decaux_bike.*
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+
+
+class LoginActivity :BaseActivity(){
+
+private val RC_SIGN_IN:Int=0
+        lateinit var auth:FirebaseAuth
+        lateinit var gso:GoogleSignInOptions
+        lateinit var googleSignInClient:GoogleSignInClient
+
+//    private val auth2: FirebaseAuth = FirebaseAuth.getInstance()
+
+        override fun onCreate(savedInstanceState:Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        auth=FirebaseAuth.getInstance()
+        auth.signOut()
+//        auth2.signOut()
+
+        val username=etFirstName
+
+
+        btnLogin.setOnClickListener{
+        userLogin()
+        }
+
+        btnGoRegister.setOnClickListener{
+        Intent(this,RegisterActivity::class.java).also{
+        startActivity(it)
+        }
+        }
+
+
+        btnRegisterGoogle.setOnClickListener{
+        signIn()
+        }
+
+
+        // Configure Google Sign In
+        gso=GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        .requestIdToken(getString(R.string.default_web_client_id))
+        .requestEmail()
+        .build();
+
+        googleSignInClient=GoogleSignIn.getClient(this,gso)
+
+
+        }
+
+private fun signIn(){
+        val signInIntent=googleSignInClient.signInIntent
+        startActivityForResult(signInIntent,RC_SIGN_IN)
+        }
+
+        override fun onActivityResult(requestCode:Int,resultCode:Int,data:Intent?){
+        super.onActivityResult(requestCode,resultCode,data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if(requestCode==RC_SIGN_IN){
+        val task=GoogleSignIn.getSignedInAccountFromIntent(data)
+        try{
+        // Google Sign In was successful, authenticate with Firebase
+        val account=task.getResult(ApiException::class.java)!!
+        Log.d("TAG","firebaseAuthWithGoogle:"+account.id)
+        firebaseAuthWithGoogle(account.idToken!!)
+
+        }catch(e:ApiException){
+        // Google Sign In failed, update UI appropriately
+        Log.w("TAG","Google sign in failed",e)
+        }
+        }
+        }
+
+private fun firebaseAuthWithGoogle(idToken:String){
+        val credential=GoogleAuthProvider.getCredential(idToken,null)
+        auth.signInWithCredential(credential)
+        .addOnCompleteListener(this){task->
+        if(task.isSuccessful){
+        // Sign in success, update UI with the signed-in user's information
+        Log.d("TAG","signInWithCredential:success")
+        val user=auth.currentUser
+        updateUI(user)
+        }else{
+        // If sign in fails, display a message to the user.
+        Toast.makeText(this,"Authentication failure",Toast.LENGTH_SHORT).show()
+        Log.w("TAG","signInWithCredential:failure",task.exception)
+        updateUI(null)
+        }
+        }
+        }
+
+        fun updateUI(user:FirebaseUser?){
+
+        }
+
+
+        // checking if the user is logged in when the App is launched
+        override fun onStart(){
+        super.onStart()
+        checkLoggedInState()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser=auth.currentUser
+        updateUI(currentUser)
+        }
+
+// function to login
+private fun userLogin(){
+        val email=etEmailInput.text.toString()
+        val password=etPasswordInput.text.toString()
+        if(email.isNotEmpty()&&password.isNotEmpty()){
+        CoroutineScope(Dispatchers.IO).launch{
+        try{
+        auth.signInWithEmailAndPassword(email,password).await()
+        withContext(Dispatchers.Main){
+        checkLoggedInState()
+        }
+        }catch(exception:Exception){
+        withContext(Dispatchers.Main){
+        Toast.makeText(
+        this@LoginActivity,
+        exception.message,
+        Toast.LENGTH_SHORT
+        )
+        .show()
+        }
+        }
+        }
+        }else{
+        CoroutineScope(Dispatchers.IO).launch{
+        withContext(Dispatchers.Main){
+        Toast.makeText(this@LoginActivity,R.string.login_error,Toast.LENGTH_SHORT)
+        .show()
+        }
+        }
+        }
+        }
+
+        // checking the log status
+        fun checkLoggedInState(){
+        if(auth.currentUser==null){
+//            tvLog.text = getString(R.string.login_error)
+        }else{
+        Intent(this,MenuActivity::class.java).also{
+        startActivity(it)
+        }
+        }
+
+        }
+
+
+        }
+
+
+
+
+
+
---
+++ app/src/main/java/com/valeriapaz/s2_21_mobile_ca4_bike_21214_22415_21643/ParisWeatherActivity.kt
@@ -0,0 +1,107 @@
+package com.valeriapaz.s2_21_mobile_ca4_bike_21214_22415_21643
+
+import android.content.Intent
+import android.os.AsyncTask
+import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_weather.*
+import org.json.JSONObject
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
+
+class ParisWeatherActivity : BaseActivity() {
+
+    val CITY: String = "Paris"
+    // Used of API key
+    val API: String = "a12c3fe55e4ee95592dab383cfbefde5"
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+
+        // Button Home handle
+        btnHome.setOnClickListener {
+            Intent(this, MenuActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+
+        // Calling Function
+        weatherTask().execute()
+
+    }
+
+    inner class weatherTask() : AsyncTask<String, Void, String>() {
+        override fun onPreExecute() {
+            super.onPreExecute()
+            // Showing the ProgressBar, Making the main design GONE
+            pbLoader.visibility = View.VISIBLE
+            tvErrorText.visibility = View.GONE
+        }
+
+        override fun doInBackground(vararg params: String?): String? {
+            var response:String?
+            try{
+                response = URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API").readText(
+                        Charsets.UTF_8
+                )
+            }catch (e: Exception){
+                response = null
+            }
+            return response
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            try {
+                // Extracting JSON from the API
+                val jsonObj = JSONObject(result)
+                val main = jsonObj.getJSONObject("main")
+                val sys = jsonObj.getJSONObject("sys")
+                val wind = jsonObj.getJSONObject("wind")
+                val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
+
+                val updatedAt:Long = jsonObj.getLong("dt")
+                val updatedAtText = "Updated at: "+ SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
+                val temp = main.getString("temp")+"°C"
+                val tempMin = "Min Temp: " + main.getString("temp_min")+"°C"
+                val tempMax = "Max Temp: " + main.getString("temp_max")+"°C"
+                val pressure = main.getString("pressure")+" psi"
+                val humidity = main.getString("humidity")+" %"
+
+                val sunrise:Long = sys.getLong("sunrise")
+                val sunset:Long = sys.getLong("sunset")
+                val windSpeed = wind.getString("speed")+" km/h"
+                val weatherDescription = weather.getString("description")
+
+                val address = jsonObj.getString("name")+", "+sys.getString("country")
+
+                // Populating extracted data into our views
+                tvLocation.text = address
+                tvUpdatedAt.text = updatedAtText
+                tvStatus.text = weatherDescription.capitalize(Locale.ROOT)
+                tvTemperature.text = temp
+                tvMinTemp.text = tempMin
+                tvMaxTemp.text = tempMax
+                tvSunriseTime.text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise*1000))
+                tvSunsetTime.text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset*1000))
+                tvWindTime.text = windSpeed
+                tvPressureTime.text = pressure
+                tvHumidityTime.text = humidity
+
+                // Views populated, Hiding the loader, Showing the main design
+                findViewById<ProgressBar>(R.id.pbLoader).visibility = View.GONE
+
+            } catch (e: Exception) {
+                findViewById<ProgressBar>(R.id.pbLoader).visibility = View.GONE
+                findViewById<TextView>(R.id.tvErrorText).visibility = View.VISIBLE
+            }
+
+        }
+    }
+
+}
